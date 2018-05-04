b2049c4d5bb5997307d727ee8e6e6f1dcd415e83
Araz Sinjary <araz@Arazs-MBP.Home>
2018-05-04 18:32:58 +0300

    create stack

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var BgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /* this is old way!
+        //Evrey frame it have position and size.
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        
+        //bgimg always be the same size and position as the view itself.
+        BgImg.frame = view.frame
+        */
     }
 
     override func didReceiveMemoryWarning() {
