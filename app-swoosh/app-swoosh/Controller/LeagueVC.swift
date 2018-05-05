66a248bf54b9589e2d9b51c58af588e4bb75b0de
Araz Sinjary <araz@Arazs-MacBook-Pro.local>
2018-05-05 21:57:30 +0300

    data model

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    //declare and make implicitly unwrapped optional.
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //initialize
+       player = Player()
     }
 
     //this programmatic segue this for more than one thing to be segue not like button segue just for one thing.
     @IBAction func onNextTapped(_ sender: Any) {
         
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    //this is a logic built but logic happen in fucn which is blow.
+    @IBAction func onMensTapped(_ sender: Any) {
+        
+        //here we will pass data.
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        
+        selectLeague(leagueType: "wonens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        
+        selectLeague(leagueType: "coed")
+    }
+    
+    //this is a logic, in IBAction we going to pass league type into this func.
+    func selectLeague(leagueType: String){
+        
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
     
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Araz Sinjary on 5/5/18.
+//  Copyright © 2018 Araz Sinjary. All rights reserved.
+//
+
+import Foundation
+
+//struct is a value type it can copied, class object stored in memory you can't copy it, also much faster than class.
+struct Player {
+    
+    //here we want to store.
+    var desiredLeague: String?
+    var selectSkillLevel: String?
+}
